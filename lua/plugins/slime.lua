9420f47cdcdd773991140606022308f9f76e4b8c
Nikhil Padmanabhan (CoCalc) <nikhil.padmanabhan@gmail.com>
2025-03-19 11:40:27 +0000

    slime update

--- lua/plugins/slime.lua
+++ lua/plugins/slime.lua
@@ -1,6 +1,16 @@
 return {
+  -- slime (REPL integration)
+  -- https://github.com/LazyVim/LazyVim/discussions/234
   {
     "jpalardy/vim-slime",
-    dev = false,
+    keys = {
+      { "<leader>rc", "<cmd>SlimeConfig<cr>", desc = "Slime Config" },
+      { "<leader>rr", "<Plug>SlimeSendCell<BAR>/^# %%<CR>", desc = "Slime Send Cell" },
+    },
+    config = function()
+      vim.g.slime_target = "tmux"
+      vim.g.slime_cell_delimiter = "#\\s\\=%%"
+      vim.g.slime_bracketed_paste = 1
+    end,
   },
 }
